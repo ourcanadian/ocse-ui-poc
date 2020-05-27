80ba8fbc50025639c1732c477c6e2124e1c40868
Rylan Cole <rylancole@MacBook-Air.hitronhub.home>
2020-05-27 15:42:21 -0700

    Grid and logo

--- app/src/components/TopBar.js
+++ app/src/components/TopBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link, useLocation } from "react-router-dom";
 
+import logo from '../images/our-white-logo.png'
+
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/ToolBar'
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 
 import { ThemeContext } from './ThemeContext'
 import SearchBar from './SearchBar'
@@ -17,13 +20,19 @@ function TopBar(props) {
                 <>
                     <AppBar style={{backgroundColor: theme.header}}>
                         <ToolBar>
-                            <Link to="/">Home</Link>
-                            {(location.pathname !== "/") && <div style={{padding: "0 100px"}}><SearchBar/></div>}
-                            <Link to="/compare">
-                                <IconButton size="small" style={{position: "fixed", right: "0"}}>
-                                    <CompareIcon/>
-                                </IconButton>
-                            </Link>
+                            <Grid
+                                container
+                                direction="row"
+                                alignItems="center"
+                            >
+                                <Link to="/"><img src={logo} alt="Our Logo" style={{maxWidth: "75px"}}/></Link>
+                                {(location.pathname !== "/") && <div style={{padding: "0 100px"}}><SearchBar/></div>}
+                                <Link to="/compare">
+                                    <IconButton edge="end">
+                                        <CompareIcon/>
+                                    </IconButton>
+                                </Link>
+                            </Grid>
                         </ToolBar>
                     </AppBar>
                     <ToolBar/>

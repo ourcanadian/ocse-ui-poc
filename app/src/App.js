72e7f42de5b0bf619d1bf423ea7597885d18c035
Rylan Cole <rylancole@MacBook-Air.hitronhub.home>
2020-05-27 12:44:52 -0700

    Page navigation

--- app/src/App.js
+++ app/src/App.js
@@ -2,24 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/compare">Compare</Link>
+      <Switch>
+        <Route path="/search">
+          Search Page
+        </Route>
+        <Route path="/compare">
+          Compare Page
+        </Route>
+        <Route path="/">
+          Home Page
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
